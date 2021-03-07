b230c20d2647018cda614c1b6e86ffb245cdc6a4
LUCAS <lucas@MacBook-Air-de-LUCAS.local>
2021-03-06 22:37:33 -0300

    rodape

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -1,11 +1,10 @@
 export default function Banner() {
   return (
     <div className="container max-w-5xl m-auto p-6 flex-1 flex flex-col justify-center items-center sm:items-start text-center sm:text-left" >     
-      <span className="text-gray">Nextjs + Tailwind</span>
       <div className="text-gray uppercase text-4xl font-extrabold leading-snug">
         Desenvolver <span className='text-green'>software</span> de verdade
       </div>
-      <span className="text-gray">Dev curioso é um dev excelente</span>
+      <span className="text-gray">DEV CURIOSO É UM DEV <span className='text-green'>EXCELENTE</span></span>
     </div>
   );
 }
--- src/Components/Rodape.js
+++ src/Components/Rodape.js
@@ -1,8 +1,14 @@
 
 export default function Rodape(){
   return(
-    <div className='container max-w-5xl text-white'>
-      a
+    <div className='container max-w-5xl text-white w-screen flex items-center justify-between flex-col md:flex-row'>
+      <div>
+        <h1 className='text-2xl uppercase font-medium p-2'>Entrar em contato</h1>
+      </div>
+      <div className='outline-none p-2'>
+        <input className='px-5 py-1 text-lg text-black rounded-2xl' placeholder='Digite seu e-mai'/>
+        <button className='bg-white px-6 py-1.5 text-black ml-4 outline-none rounded-2xl'>Enviar</button>
+      </div>
     </div>
   )
 }
